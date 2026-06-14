82c4fce623c8300ddf095296e0c02a8f03696b76
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2026-06-14 16:16:04 +0300

    Implement erase and move methods for XDataBuffer

--- engine/src/data_buffer.cpp
+++ engine/src/data_buffer.cpp
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "buffer.hpp"
+#include "data_buffer.hpp"
 #include "capabilities.hpp"
 #include "context.hpp"
 #include "engine.hpp"
@@ -34,9 +34,23 @@ triton::XDataBuffer::~XDataBuffer()
 void triton::XDataBuffer::Write(const u8* data, usize byteSize, usize byteOffset)
 {
 	if (data == nullptr || byteSize >= _byteSize || byteOffset >= _byteSize)
-	{
 		return;
-	}
 
 	memcpy(&_data[byteOffset], data, byteSize);
+}
+
+void triton::XDataBuffer::Erase(usize byteSize, usize byteOffset)
+{
+	if (byteSize >= _byteSize || byteOffset >= _byteSize || byteOffset + byteSize >= _byteSize)
+		return;
+
+	memcpy(&_data[byteOffset], &_data[byteOffset + byteSize], _byteSize - (byteOffset + byteSize));
+}
+
+void triton::XDataBuffer::Move(types::usize byteSize, types::usize sourceByteOffset, types::usize destinationByteOffset)
+{
+	if (byteSize >= _byteSize || sourceByteOffset >= _byteSize || destinationByteOffset >= _byteSize || sourceByteOffset + byteSize >= _byteSize)
+		return;
+
+	memcpy(&_data[destinationByteOffset], &_data[sourceByteOffset], byteSize);
 }
