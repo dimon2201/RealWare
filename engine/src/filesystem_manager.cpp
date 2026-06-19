c50aa149fe02993c9a32337763519e1f0eeb35a2
dmsi190 <dmytrii4real@gmail.com>
2026-06-19 20:28:16 +0300

    Fix error in cDataFile constructor call

--- engine/src/filesystem_manager.cpp
+++ engine/src/filesystem_manager.cpp
@@ -52,7 +52,7 @@ triton::cDataFile* triton::cFileSystem::CreateDataFile(const std::string& path, 
 
 std::string triton::cFileSystem::TextFileToString(const std::string& path)
 {
-    cDataFile* file = _context->Create<cDataFile>(_context, path);
+    cDataFile* file = _context->Create<cDataFile>(_context, path, K_TRUE);
     std::string str = "";
     for (usize i = 0; i < file->GetBuffer()->GetByteSize(); i++)
         str.push_back(((u8*)file->GetData())[i]);
