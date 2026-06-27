6562b849a0755d1da486991f06e8db868070e968
dmsi190 <dmytrii4real@gmail.com>
2026-06-27 14:53:55 +0300

    Define constructor and destructor for XRenderBatch

--- engine/src/render_batch.cpp
+++ engine/src/render_batch.cpp
@@ -1,7 +1,16 @@
 // render_batch.cpp
 
 #include "render_batch.hpp"
+#include "context.hpp"
 
-triton::CRenderBatch::CRenderBatch(const SGeometryView& geometry) : _geometry(geometry)
+triton::XRenderBatch::XRenderBatch(cContext* context, const SGeometryView& geometry) : iObject(context), _geometry(geometry)
 {
+	_staticInstances = _context->Create<XInstanceBuffer>(_context);
+	_dynamicInstances = _context->Create<XInstanceBuffer>(_context);
+}
+
+triton::XRenderBatch::~XRenderBatch()
+{
+	_context->Destroy<XInstanceBuffer>(_dynamicInstances);
+	_context->Destroy<XInstanceBuffer>(_staticInstances);
 }
