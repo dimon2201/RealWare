df9dc15be5f785dbb2b29d5c22c5e0f7d8d86caa
dmsi190 <dmytrii4real@gmail.com>
2026-06-30 10:47:55 +0300

    Realize basic XMaterialSubsystem interface

---
+++ engine/src/material_subsystem.cpp
@@ -0,0 +1,26 @@
+// material_subsystem.cpp
+
+#include "material_subsystem.hpp"
+
+using namespace types;
+
+void triton::XMaterialSubsystem::Init()
+{
+}
+
+void triton::XMaterialSubsystem::Free()
+{
+}
+
+void triton::XMaterialSubsystem::Update()
+{
+	WriteDirty();
+}
+
+void triton::XMaterialSubsystem::MarkDirty()
+{
+}
+
+void triton::XMaterialSubsystem::WriteDirty()
+{
+}
