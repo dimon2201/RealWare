5ab30e3a55464778de365a75a65d789e5674d226
dmsi190 <dmytrii4real@gmail.com>
2026-06-30 20:21:56 +0300

    Define basic STexture fields

--- engine/src/texture.hpp
+++ engine/src/texture.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "math.hpp"
+#include "types.hpp"
 
 namespace triton
 {
@@ -11,6 +12,10 @@ namespace triton
 	struct STexture
 	{
 		cTexture* gpuInstance = nullptr;
-		cVector2 size = cVector2(0.0f);
+		types::u32 layer = 0;
+		cVector2 normOffset = cVector2(0.0f);
+		cVector2 normSize = cVector2(0.0f);
+		cVector2 pixelOffset = cVector2(0.0f);
+		cVector2 pixelSize = cVector2(0.0f);
 	};
 }
