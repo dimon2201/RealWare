a095b2e327d8d8c4bb99004a9ee5b364d91aed47
dmsi190 <dmytrii4real@gmail.com>
2026-07-06 09:40:45 +0300

    Initialize XEditor object and run it

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "context.hpp"
+#include "editor.hpp"
+
+using namespace triton;
+using namespace triton::editor;
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	cContext* context = new cContext();
+	XEditor* editor = new XEditor(context);
+	editor->Initialize();
+	editor->Run();
+	editor->Shutdown();
+	delete editor;
+	delete context;
 
 	return 0;
 }
