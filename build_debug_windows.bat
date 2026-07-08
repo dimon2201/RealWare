bd1a4404a2665825301e9b12a200b2d859cd9ecf
dmsi190 <dmytrii4real@gmail.com>
2026-07-08 20:42:51 +0300

    Fix .bat build files

--- build_debug_windows.bat
+++ build_debug_windows.bat
@@ -1,13 +1,15 @@
 @echo off
 
 echo Building TritonEngine... (Windows OS, MSVC compiler)
-call build_engine_debug_win32.bat
+call build_debug_engine_windows.bat
 echo TritonEngine built.
 
 echo.
 echo Building TritonEditor... (Windows OS, MSVC compiler)
-call build_editor_debug_win32.bat
+call build_debug_editor_windows.bat
 echo TritonEditor built.
 
 echo.
 echo Build completed.
+
+pause
