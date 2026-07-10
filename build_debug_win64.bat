c64b662ffacefc1d77fcf3a49e20c5439f83b2a7
dmsi190 <dmytrii4real@gmail.com>
2026-07-10 17:30:03 +0300

    Update "build_debug_win64.bat"

--- build_debug_win64.bat
+++ build_debug_win64.bat
@@ -1,16 +1,11 @@
 @echo off
 
 set PRESET_NAME=debug_win64
-set SOURCE_DIR=.
-set BUILD_DIR=build\%PRESET_NAME%
-set GENERATOR="Visual Studio 17 2022"
 
 git submodule update --init --recursive
-rem rmdir /S /Q %BUILD_DIR%
-cmake ^
-    -S %SOURCE_DIR% ^
-    -B %BUILD_DIR% ^
-    -G %GENERATOR%
-cmake --build %BUILD_DIR% --preset %PRESET_NAME%
+
+rem rmdir /S /Q build
+cmake --preset %PRESET_NAME%
+cmake --build --preset %PRESET_NAME%
 
 pause
