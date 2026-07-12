a9193254913287b5d5016b3bde75623da9b5638f
dmsi190 <dmytrii4real@gmail.com>
2026-07-12 16:57:19 +0300

    Move animation related structs to separate file

---
+++ engine/src/animation.hpp
@@ -0,0 +1,40 @@
+// animation.hpp
+
+#pragma once
+
+namespace triton
+{
+    struct SBonePositionKey
+    {
+        types::f32 time = 0.0f;
+        cVector3 position = cVector3(0.0f);
+    };
+
+    struct SBoneRotationKey
+    {
+        types::f32 time = 0.0f;
+        cQuaternion rotation = cQuaternion(0.0f, 0.0f, 0.0f, 0.0f);
+    };
+
+    struct SBoneScaleKey
+    {
+        types::f32 time = 0.0f;
+        cVector3 scale = cVector3(0.0f);
+    };
+
+    struct SBoneAnimation
+    {
+        types::usize boneIndex = 0;
+        std::vector<SBonePositionKey> positionKeys = {};
+        std::vector<SBoneRotationKey> rotationKeys = {};
+        std::vector<SBoneScaleKey> scaleKeys = {};
+    };
+
+    struct SAnimation
+    {
+        std::string name = "";
+        types::f32 duration = 0.0f;
+        types::f32 ticksPerSecond = 0.0f;
+        std::vector<SBoneAnimation> bones = {};
+    };
+}
