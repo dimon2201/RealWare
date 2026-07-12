2e5fe8c0192780226f9978b2a55e8a1952384ce3
dmsi190 <dmytrii4real@gmail.com>
2026-07-12 17:31:47 +0300

    Add boneBufferSize capability

--- engine/src/capabilities.hpp
+++ engine/src/capabilities.hpp
@@ -36,6 +36,7 @@ namespace triton
         types::usize maxRenderDynamicInstanceCount = 32 * 1024;
         types::usize vertexBufferSize = 64 * 1024 * 1024;
         types::usize indexBufferSize = 64 * 1024 * 1024;
+        types::usize boneBufferSize = 64 * 1024 * 1024;
         types::usize hashTableChunkByteSize = 16 * 1024;
         types::usize hashTableMaxChunkCount = 256;
         types::usize hashTableSize = 4096;
