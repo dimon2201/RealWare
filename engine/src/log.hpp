b2ba7376035eb48986a5ae38b31413411022402f
dmsi190 <dmytrii4real@gmail.com>
2026-07-15 18:55:26 +0300

    Make log functions inline

--- engine/src/log.hpp
+++ engine/src/log.hpp
@@ -3,18 +3,24 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
 
 namespace triton
 {
 	template<typename T>
-	void Print(const T& message)
+	inline void Print(const T& message)
 	{
 		std::cout << message << std::endl;
 	}
 
 	template<typename T>
-	void Print(const T& message, std::ostream& stream)
+	inline void Print(const T& message, std::ostream& stream)
 	{
 		stream << message << std::endl;
+	}
+
+	inline void Print(std::ostringstream& stream)
+	{
+		std::cout << stream.str();
 	}
 }
