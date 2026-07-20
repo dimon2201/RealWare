7a6049628b572a4175213b8aa3a579f1a1a15cab
dmsi190 <dmytrii4real@gmail.com>
2026-07-20 18:36:48 +0300

    feat: Add CLI for TAsset Tool

--- tools/tasset/src/tool/tasset_tool.cpp
+++ tools/tasset/src/tool/tasset_tool.cpp
@@ -2,14 +2,61 @@
 
 using namespace triton::asset;
 
+void PrintHelp()
+{
+	Print("tasset - Triton Asset Tool");
+	Print("");
+	Print("Usage:");
+	Print("  tasset <command> <input> <output>");
+	Print("");
+	Print("Commands:");
+	Print("  -c     Convert");
+	Print("  -h     Show help message");
+	Print("");
+	Print("Examples:");
+	Print("  tasset -c knight.fbx knight.tasset");
+}
+
 int main(int argc, char** argv)
 {
-	CModel3DAsset resource;
-	resource.LoadRawFile("C:/My/My_Projects_Theoretical/Game_TerkonProtocol/Building5_Dif.fbx");
-	resource.WriteAssetFile("C:/My/My_Projects_Theoretical/Game_TerkonProtocol/Building5_Dif.trf");
-	resource.LoadAssetFile("C:/My/My_Projects_Theoretical/Game_TerkonProtocol/Building5_Dif.trf");
+	if (argc > 1)
+	{
+		std::string flag = std::string(argv[1]);
 
-	system("pause");
+		if (flag == "-h")
+		{
+			PrintHelp();
+			return 0;
+		}
+		else if (flag == "-c")
+		{
+			if (argc == 4)
+			{
+				const std::filesystem::path inFilePath = std::filesystem::path(argv[2]);
+				if (inFilePath.extension() == ".fbx")
+				{
+					Print("Info: converting FBX 3d model file...");
+
+					const std::string inFilePathStr = inFilePath.generic_string();
+					const std::string outFilePathStr = std::string(argv[3]);
+
+					CModel3DAsset asset;
+					asset.LoadRawFile(inFilePathStr);
+					asset.WriteAssetFile(outFilePathStr);
+
+					Print("Info: file was converted successfully!");
+
+					return 0;
+				}
+			}
+			else
+			{
+				Print("Error: insufficient argument count");
+			}
+		}
+	}
+
+	PrintHelp();
 
 	return 0;
 }
