8fd162bf036439d9609bf0f15309ce01890bebdb
dmsi190 <dmytrii4real@gmail.com>
2026-08-02 17:30:57 +0300

    feat: Add CPool with basic interface

---
+++ engine/src/object_pool.hpp
@@ -0,0 +1,20 @@
+// object_pool.hpp
+
+#pragma once
+
+#include "types.hpp"
+
+namespace triton
+{
+	class CPool final
+	{
+	public:
+		static void Initialize()
+		{
+		}
+
+		static void Free()
+		{
+		}
+	};
+}
