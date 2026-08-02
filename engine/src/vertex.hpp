279ddaae9ed87f9005fc10b4b80286bc2ae6fd5f
dmsi190 <dmytrii4real@gmail.com>
2026-08-02 17:38:02 +0300

    feat: Update SStaticVertex scope

--- engine/src/vertex.hpp
+++ engine/src/vertex.hpp
@@ -3,12 +3,22 @@
 #pragma once
 
 #include "math.hpp"
+#include "handle.hpp"
 #include "types.hpp"
 
 namespace triton
 {
 	struct SStaticVertex
 	{
+		struct THandle : public SHandle {};
+		struct TGPULayout
+		{
+			cVector3 position = cVector3(0.0f);
+			cVector2 texcoord = cVector2(0.0f);
+			cVector3 normal = cVector3(0.0f);
+			types::s32 materialIndex = -1;
+		};
+
 		cVector3 position = cVector3(0.0f);
 		cVector2 texcoord = cVector2(0.0f);
 		cVector3 normal = cVector3(0.0f);
