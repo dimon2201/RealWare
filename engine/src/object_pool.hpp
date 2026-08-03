af815a4d39fdb3b26f270b7b7e020f9f5ee744eb
dmsi190 <dmytrii4real@gmail.com>
2026-08-03 16:01:25 +0300

    feat: Create XStaticVertexPool in CPool::Initialize()

--- engine/src/object_pool.hpp
+++ engine/src/object_pool.hpp
@@ -2,15 +2,25 @@
 
 #pragma once
 
+#include "object_allocator.hpp"
+#include "static_vertex_pool.hpp"
 #include "types.hpp"
 
 namespace triton
 {
 	class CPool final
 	{
+		XStaticVertexPool* _staticVertexPool = nullptr;
+
 	public:
-		static void Initialize()
+		static void Initialize(cContext* context)
 		{
+			_staticVertexPool = CObjectAllocator::Create<XStaticVertexPool>(
+				context,
+				types::K_TRUE,
+				0,
+				cBuffer::eType::VERTEX
+			);
 		}
 
 		static void Free()
