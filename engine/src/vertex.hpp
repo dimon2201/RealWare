4b7640f304650813f8d03084b9d6414e2abf12da
dmsi190 <dmytrii4real@gmail.com>
2026-08-06 15:54:59 +0300

    feat: Add SStaticVertexGPULayout and SSkinnedVertexGPULayout

--- engine/src/vertex.hpp
+++ engine/src/vertex.hpp
@@ -8,21 +8,23 @@
 
 namespace triton
 {
-	struct SStaticVertex
+	struct SStaticVertexGPULayout final
 	{
-		struct THandle : public SHandle {};
-		struct TGPULayout
-		{
-			cVector3 position = cVector3(0.0f);
-			cVector2 texcoord = cVector2(0.0f);
-			cVector3 normal = cVector3(0.0f);
-			types::s32 materialIndex = -1;
-		};
-
 		cVector3 position = cVector3(0.0f);
 		cVector2 texcoord = cVector2(0.0f);
 		cVector3 normal = cVector3(0.0f);
 		types::s32 materialIndex = -1;
+	};
+
+	struct SSkinnedVertexGPULayout final
+	{
+		cVector3 position = cVector3(0.0f);
+		cVector2 texcoord = cVector2(0.0f);
+		cVector3 normal = cVector3(0.0f);
+		cVector4 tangent = cVector4(0.0f);
+		types::s32 materialIndex = -1;
+		types::u32 boneIndices[4] = {};
+		types::f32 boneWeights[4] = {};
 	};
 
 	struct SVertex
