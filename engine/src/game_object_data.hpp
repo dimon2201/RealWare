fe2adca0dcc341feb454309db1237a58658ae0cf
dmsi190 <dmytrii4real@gmail.com>
2026-08-08 16:55:06 +0300

    feat: Update "game_object_data.hpp"

---
+++ engine/src/game_object_data.hpp
@@ -0,0 +1,29 @@
+// game_object_data.hpp
+
+#pragma once
+
+#include <string>
+#include "batch_instance.hpp"
+#include "handle.hpp"
+#include "material.hpp"
+#include "math.hpp"
+
+namespace triton
+{
+    struct SGameObjectData
+    {
+        struct THandle : public SHandle {};
+
+        struct TGPULayout {};
+
+        std::string name = {};
+        ERenderInstanceMotionType motionType = ERenderInstanceMotionType::Static;
+        HStaticRenderInstance staticRenderInstance;
+        HDynamicRenderInstance dynamicRenderInstance;
+        HMaterial material;
+        cVector3 worldPosition = cVector3(0.0f);
+        cVector3 worldRotation = cVector3(0.0f);
+        cVector3 scale = cVector3(1.0f);
+        HSkeleton skeleton = {};
+    };
+}
