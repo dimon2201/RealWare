ec1425087ef8ef69a97a52d142566eb71d9420c0
dmsi190 <dmytrii4real@gmail.com>
2026-08-09 18:40:22 +0300

    feat: Rename "material.hpp"

---
+++ engine/src/material_data.hpp
@@ -0,0 +1,39 @@
+// material_data.hpp
+
+#pragma once
+
+#include "math.hpp"
+#include "handles.hpp"
+#include "texture_subsystem.hpp"
+#include "texture_data.hpp"
+
+namespace triton
+{
+    struct STextureLayout
+    {
+        types::u32 layer = 0;
+        types::u32 _pad = 0;
+        cVector2 normOffset = cVector2(0.0f);
+        cVector2 normSize = cVector2(0.0f);
+    };
+
+    struct SMaterialData
+    {
+        struct THandle : public SHandle {};
+
+        struct TGPULayout
+        {
+            STextureLayout diffuseTextureLayout;
+            STextureLayout normalTextureLayout;
+            STextureLayout roughnessTextureLayout;
+            STextureLayout metallicTextureLayout;
+            cVector4 diffuseColor = cVector4(0.0f);
+        };
+
+        STextureData::THandle diffuseTexture;
+        STextureData::THandle normalTexture;
+        STextureData::THandle roughnessTexture;
+        STextureData::THandle metallicTexture;
+        cVector4 diffuseColor = cVector4(0.0f);
+    };
+}
