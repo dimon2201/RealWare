649212ccf5d56ac56c87e3d6d382f80ea2dc4315
dmsi190 <dmytrii4real@gmail.com>
2026-08-09 18:41:35 +0300

    feat: Rewrite material subsystem to use pool

--- engine/src/material_subsystem.cpp
+++ engine/src/material_subsystem.cpp
@@ -6,31 +6,27 @@
 #include "engine.hpp"
 #include "capabilities.hpp"
 #include "texture_subsystem.hpp"
+#include "material_pool.hpp"
 
 using namespace types;
 
-triton::XMaterialSubsystem::XMaterialSubsystem(cContext* context)
-    : ISubsys(context),
-      CUploader<SMaterial, HMaterial, XLinearArray<SMaterial>, SGPUMaterialLayout>(
-          context,
-          (cGPUResource**)&_materialGPUBuffer,
-          context->GetSubsystem<cEngine>()->GetCapabilities()->maxRenderMaterialCount,
-          K_FALSE
-      )
+triton::XMaterialSubsystem::XMaterialSubsystem(cContext* context) : ISubsys(context)
 {
-    const sCapabilities* caps = context->GetSubsystem<cEngine>()->GetCapabilities();
-
-    _context->GetSubsystem<cEngine>()->GetRenderCommandRecorder()->PushCommand(SRenderCommand(
-        ERenderCommand::CREATE_BUFFER,
-        (cpuword)cBuffer::eType::STORAGE,
-        (cpuword)nullptr,
-        caps->maxRenderMaterialCount * sizeof(SGPUMaterialLayout),
-        2
-    ));
-    _materialGPUBuffer = _context->GetSubsystem<cEngine>()->GetSynchronization()->WaitForRenderCommandResult<cBuffer*>();
+    _pool = CObjectAllocator::Create<XMaterialPool>(
+        64,
+        _context,
+        K_TRUE,
+        (s32)2,
+        cBuffer::eType::STORAGE
+    );
 }
 
-triton::HMaterial triton::XMaterialSubsystem::CreateMaterial(
+triton::XMaterialSubsystem::~XMaterialSubsystem()
+{
+    CObjectAllocator::Destroy<XMaterialPool>(_pool);
+}
+
+std::optional<triton::SMaterialData::THandle> triton::XMaterialSubsystem::Create(
     const cVector4& diffuseColor,
     const HTexture& diffuseTexture,
     const HTexture& normalTexture,
@@ -38,61 +34,27 @@ triton::HMaterial triton::XMaterialSubsystem::CreateMaterial(
     const HTexture& metallicTexture
 )
 {
-    HMaterial material = Create();
+    auto handleResult = _pool->Create();
+    if (!handleResult.has_value())
+        return std::nullopt;
+    auto handle = *handleResult;
 
-    SMaterial& m = Get(material);
+    SMaterialData& m = *_pool->Get(handle);
     m.diffuseColor = diffuseColor;
     m.diffuseTexture = diffuseTexture;
     m.normalTexture = normalTexture;
     m.roughnessTexture = roughnessTexture;
     m.metallicTexture = metallicTexture;
 
-    SGPUMaterialLayout gml = ConvertToGPULayout(material);
-    WriteToStaging(
-        GetHandleBufferIndex(material),
-        &gml,
-        1
+    _pool->WriteToStaging(
+        *_pool->GetBufferIndex(handle),
+        m
     );
 
-    return material;
-}
-
-void triton::XMaterialSubsystem::Init()
-{
-}
-
-void triton::XMaterialSubsystem::Free()
-{
+    return handle;
 }
 
 void triton::XMaterialSubsystem::Update()
 {
-    UploadStagingToGpuIfDirty(_context->GetSubsystem<cEngine>()->GetRenderCommandRecorder());
-}
-
-triton::SGPUMaterialLayout triton::XMaterialSubsystem::ConvertToGPULayout(const HMaterial& material)
-{
-    SMaterial& m = Get(material);
-
-    STexture& dif = _context->GetSubsystem<XTextureSubsystem>()->Get(m.diffuseTexture);
-    STexture& nor = _context->GetSubsystem<XTextureSubsystem>()->Get(m.normalTexture);
-    STexture& rgh = _context->GetSubsystem<XTextureSubsystem>()->Get(m.roughnessTexture);
-    STexture& met = _context->GetSubsystem<XTextureSubsystem>()->Get(m.metallicTexture);
-
-    SGPUMaterialLayout gml;
-    gml.diffuseColor = m.diffuseColor;
-    gml.diffuseTextureLayout.layer = dif.layer;
-    gml.diffuseTextureLayout.normOffset = dif.normOffset;
-    gml.diffuseTextureLayout.normSize = dif.normSize;
-    gml.normalTextureLayout.layer = nor.layer;
-    gml.normalTextureLayout.normOffset = nor.normOffset;
-    gml.normalTextureLayout.normSize = nor.normSize;
-    gml.roughnessTextureLayout.layer = rgh.layer;
-    gml.roughnessTextureLayout.normOffset = rgh.normOffset;
-    gml.roughnessTextureLayout.normSize = rgh.normSize;
-    gml.metallicTextureLayout.layer = met.layer;
-    gml.metallicTextureLayout.normOffset = met.normOffset;
-    gml.metallicTextureLayout.normSize = met.normSize;
-
-    return gml;
+    _pool->Update();
 }
--- engine/src/material_subsystem.hpp
+++ engine/src/material_subsystem.hpp
@@ -3,32 +3,30 @@
 #pragma once
 
 #include "object.hpp"
-#include "subsystem.hpp"
-#include "game_object.hpp"
+#include "game_object_data.hpp"
 #include "handles.hpp"
 #include "graphics_buffer_formats.hpp"
 #include "render_instance_data.hpp"
-#include "material.hpp"
+#include "material_data.hpp"
 #include "types.hpp"
 
 #include "DELETE_THIS_FILE_ASAP.hpp"
 
 namespace triton
 {
-    class XMaterialUploader;
+    class XMaterialPool;
 
-    class XMaterialSubsystem : public ISubsys,
-                               public CUploader<SMaterial, HMaterial, XLinearArray<SMaterial>, SGPUMaterialLayout>
+    class XMaterialSubsystem : public ISubsys
     {
         TRITON_OBJECT(XMaterialSubsystem)
 
-        cBuffer* _materialGPUBuffer = nullptr;
+        XMaterialPool* _pool = nullptr;
 
     public:
         explicit XMaterialSubsystem(cContext* context);
-        ~XMaterialSubsystem() override = default;
+        ~XMaterialSubsystem() override;
 
-        HMaterial CreateMaterial(
+        std::optional<SMaterialData::THandle> Create(
             const cVector4& diffuseColor,
             const HTexture& diffuseTexture,
             const HTexture& normalTexture,
@@ -36,22 +34,12 @@ namespace triton
             const HTexture& metallicTexture
         );
 
-        void DestroyMaterial(const HMaterial& material);
+        void Destroy(const SMaterialData::THandle& material);
 
-        void Set(types::usize materialIndex, const SMaterial& materialData);
+        void Init() override {}
 
-        void Init() override;
-
-        void Free() override;
+        void Free() override {}
 
         void Update() override;
-
-        inline cBuffer& GetMaterialGPUBuffer() const
-        {
-            return *_materialGPUBuffer;
-        }
-
-    private:
-        SGPUMaterialLayout ConvertToGPULayout(const HMaterial& material);
     };
 }
