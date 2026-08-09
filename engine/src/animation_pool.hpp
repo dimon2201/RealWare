efd043d17bb132b0faa59f225dba86de24df311f
dmsi190 <dmytrii4real@gmail.com>
2026-08-09 11:17:07 +0300

    feat: Add XAnimationPool

---
+++ engine/src/animation_pool.hpp
@@ -0,0 +1,27 @@
+// animation_pool.hpp
+
+#pragma once
+
+#include "object_pool_base.hpp"
+
+namespace triton
+{
+    class XAnimationPool : public XObjectPoolBase<SAnimationData>
+    {
+    public:
+        using XObjectPoolBase<SAnimationData>::XObjectPoolBase;
+        ~XAnimationPool() override = default;
+
+        SAnimationData::TGPULayout ConvertToGpuLayout(const SAnimationData& object) override
+        {
+            SAnimationData::TGPULayout gpul;
+
+            return gpul;
+        }
+
+        void Update() override
+        {
+            Upload();
+        }
+    };
+}
