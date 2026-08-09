b4c920e9ee09010083c2cba3f14dd0985267823b
dmsi190 <dmytrii4real@gmail.com>
2026-08-09 11:29:24 +0300

    feat: Rename "skeleton.hpp"

--- engine/src/skeleton_data.hpp
+++ engine/src/skeleton_data.hpp
@@ -1,20 +1,23 @@
-// skeleton.hpp
+// skeleton_data.hpp
 
 #pragma once
 
 #include <vector>
 #include "bone.hpp"
 #include "math.hpp"
+#include "handle.hpp"
 
 namespace triton
 {
-	struct SGPUSkeletonLayout
-	{
-		types::u32 globSkinnedBoneBufferOffset;
-	};
-
 	struct SSkeletonData
 	{
+		struct THandle : public SHandle {};
+
+		struct TGPULayout
+		{
+			types::u32 globSkinnedBoneBufferOffset;
+		};
+
 		types::u32 globSkinnedBoneBufferOffset = 0;
 		cMatrix4 accumulatedRootTransform = cMatrix4();
 		std::vector<SBone> bones = {};
