f8cff8840b315d9398558109a249f233d223362a
dmsi190 <dmytrii4real@gmail.com>
2026-08-12 12:38:04 +0300

    feat: Move out batch creation from game object subsystem

--- engine/src/game_object_subsystem.cpp
+++ engine/src/game_object_subsystem.cpp
@@ -57,12 +57,7 @@ void triton::XGameObjectSubsystem::Update()
 
 std::optional<triton::SStaticRenderInstanceData::THandle> triton::XGameObjectSubsystem::SetRenderableStatic(
 	const SGameObjectData::THandle& gameObject,
-	EVertexBufferFormat format,
-	const types::u8* vertexBytes,
-	types::usize vertexBytesCount,
-	const types::u8* indexBytes,
-	types::usize indexBytesCount,
-	const std::optional<SBatchData::THandle>& existingBatch,
+	const SBatchData::THandle& batch,
 	const std::optional<SMaterialData::THandle>& existingMaterial
 )
 {
@@ -73,25 +68,8 @@ std::optional<triton::SStaticRenderInstanceData::THandle> triton::XGameObjectSub
 		return std::nullopt;
 	auto value = *valueResult;
 
-	SBatchData::THandle batch;
-	if (!existingBatch.has_value())
-	{
-		SGeometryView geometry = *_context->GetSubsystem<XGeometryStorage>()->Create(
-			format,
-			vertexBytes,
-			vertexBytesCount,
-			indexBytes,
-			indexBytesCount
-		);
-		batch = *batchSubsystem->Create(format, ERenderInstanceMotionType::Static, geometry);
-	}
-	else
-	{
-		batch = *existingBatch;
-	}
-	
 	SStaticRenderInstanceData::THandle rih = *batchSubsystem->AddStaticInstance(batch, gameObject);
-
+	
 	if (existingMaterial.has_value())
 	{
 		auto ri = *batchSubsystem->GetStaticRenderInstancePool()->Get(rih);
@@ -101,64 +79,9 @@ std::optional<triton::SStaticRenderInstanceData::THandle> triton::XGameObjectSub
 	return rih;
 }
 
-std::optional<triton::SStaticRenderInstanceData::THandle> triton::XGameObjectSubsystem::SetRenderableStatic(
-	const SGameObjectData::THandle& gameObject,
-	const SModel3DData::THandle& model,
-	const std::optional<SBatchData::THandle>& existingBatch,
-	const std::optional<SMaterialData::THandle>& existingMaterial
-)
-{
-	auto valueResult = _pool->Get(gameObject);
-	if (!valueResult.has_value())
-		return std::nullopt;
-	auto value = *valueResult;
-
-	SModel3DData& m3dd = *_context->GetSubsystem<XModel3DSubsystem>()->GetPool()->Get(model);
-	
-	SBatchData::THandle batch;
-	if (!existingBatch.has_value())
-	{
-		// TODO: Use proper vertex buffer format here
-		// ||||||||||||||||||||||||||||||||||||||||||
-		// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-		SGeometryView geometry = *_context->GetSubsystem<XGeometryStorage>()->Create(
-			EVertexBufferFormat::Skinned_84,
-			(u8*)m3dd.vertexData,
-			m3dd.vertexCount * sizeof(SStaticVertexGPULayout),
-			(u8*)m3dd.indexData,
-			m3dd.indexCount * sizeof(u32)
-		);
-		batch = *_context->GetSubsystem<XBatchSubsystem>()->Create(EVertexBufferFormat::Skinned_84, ERenderInstanceMotionType::Static, geometry);
-	}
-	else
-	{
-		batch = *existingBatch;
-	}
-
-	SStaticRenderInstanceData::THandle rih = *_context->GetSubsystem<XBatchSubsystem>()->AddStaticInstance(batch, gameObject);
-
-	if (existingMaterial.has_value())
-	{
-		auto ri = *_context->GetSubsystem<XBatchSubsystem>()->GetStaticRenderInstancePool()->Get(rih);
-		ri.get().material = *existingMaterial;
-	}
-	else
-	{
-		auto ri = *_context->GetSubsystem<XBatchSubsystem>()->GetStaticRenderInstancePool()->Get(rih);
-		ri.get().material.Invalidate();
-	}
-
-	return rih;
-}
-
 std::optional<triton::SDynamicRenderInstanceData::THandle> triton::XGameObjectSubsystem::SetRenderableDynamic(
 	const SGameObjectData::THandle& gameObject,
-	EVertexBufferFormat format,
-	const types::u8* vertexBytes,
-	types::usize vertexBytesCount,
-	const types::u8* indexBytes,
-	types::usize indexBytesCount,
-	const std::optional<SBatchData::THandle>& existingBatch,
+	const SBatchData::THandle& batch,
 	const std::optional<SMaterialData::THandle>& existingMaterial
 )
 {
@@ -169,80 +92,12 @@ std::optional<triton::SDynamicRenderInstanceData::THandle> triton::XGameObjectSu
 		return std::nullopt;
 	auto value = *valueResult;
 
-	SBatchData::THandle batch;
-	if (!existingBatch.has_value())
-	{
-		SGeometryView geometry = *_context->GetSubsystem<XGeometryStorage>()->Create(
-			format,
-			vertexBytes,
-			vertexBytesCount,
-			indexBytes,
-			indexBytesCount
-		);
-		batch = *batchSubsystem->Create(format, ERenderInstanceMotionType::Dynamic, geometry);
-	}
-	else
-	{
-		batch = *existingBatch;
-	}
-
 	SDynamicRenderInstanceData::THandle rih = *batchSubsystem->AddDynamicInstance(batch, gameObject);
-
+	
 	if (existingMaterial.has_value())
 	{
 		auto ri = *_context->GetSubsystem<XBatchSubsystem>()->GetDynamicRenderInstancePool()->Get(rih);
 		ri.get().material = *existingMaterial;
-	}
-
-	return rih;
-}
-
-std::optional<triton::SDynamicRenderInstanceData::THandle> triton::XGameObjectSubsystem::SetRenderableDynamic(
-	const SGameObjectData::THandle& gameObject,
-	const SModel3DData::THandle& model,
-	const std::optional<SBatchData::THandle>& existingBatch,
-	const std::optional<SMaterialData::THandle>& existingMaterial
-)
-{
-	auto valueResult = _pool->Get(gameObject);
-	if (!valueResult.has_value())
-		return std::nullopt;
-	auto value = *valueResult;
-
-	SModel3DData& m3dd = *_context->GetSubsystem<XModel3DSubsystem>()->GetPool()->Get(model);
-
-	SBatchData::THandle batch;
-	if (!existingBatch.has_value())
-	{
-		// TODO: Use proper vertex buffer format here
-		// ||||||||||||||||||||||||||||||||||||||||||
-		// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-		SGeometryView geometry = *_context->GetSubsystem<XGeometryStorage>()->Create(
-			EVertexBufferFormat::Skinned_84,
-			(u8*)m3dd.vertexData,
-			m3dd.vertexCount * sizeof(SSkinnedVertexGPULayout),
-			(u8*)m3dd.indexData,
-			m3dd.indexCount * sizeof(u32)
-		);
-		batch = *_context->GetSubsystem<XBatchSubsystem>()->Create(EVertexBufferFormat::Skinned_84, ERenderInstanceMotionType::Dynamic, geometry);
-	}
-	else
-	{
-		batch = *existingBatch;
-	}
-
-	SDynamicRenderInstanceData::THandle rih = *_context->GetSubsystem<XBatchSubsystem>()->AddDynamicInstance(batch, gameObject);
-
-	auto ri = *_context->GetSubsystem<XBatchSubsystem>()->GetDynamicRenderInstancePool()->Get(rih);
-	ri.get().skeleton = m3dd.skeleton;
-
-	if (existingMaterial.has_value())
-	{
-		ri.get().material = *existingMaterial;
-	}
-	else
-	{
-		ri.get().material.Invalidate();
 	}
 
 	return rih;
