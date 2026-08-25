c7964c6a19152c5dad1258a16820d7224561cfbe
dmsi190 <dmytrii4real@gmail.com>
2026-08-25 12:05:59 +0300

    feat: Add ENativeRenderCommand

---
+++ engine/src/render_native_command_enum.hpp
@@ -0,0 +1,19 @@
+// render_native_command_enum.hpp
+
+#pragma once
+
+#include "types.hpp"
+
+namespace triton
+{
+	enum class ENativeRenderCommand : types::dword
+	{
+		Unknown,
+		BeginRenderPass,
+		BindPipeline,
+		SetViewport,
+		SetScissor,
+		Draw,
+		EndRenderPass
+	};
+}
